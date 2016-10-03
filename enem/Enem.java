26f13111d3a63a0fb40100f2f7e4c683b60b8779
Igor Carvalho Guimarães <igor2311@gmail.com>
2016-10-02 21:43:27 -0300

    Update Enem.java

--- enem/Enem.java
+++ enem/Enem.java
@@ -1,23 +1,27 @@
-package enem;
+package pkgEnem;
+
 import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
- * Autor : Igor Carvalho Guimarães
- * Matricula : 15.1.8382
+ * Autor : Igor Carvalho Guimarães Matricula : 15.1.8382
  */
 public class Enem {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        comandos cmd = new comandos();
-        String valor = null;
-        String cursos = null;
-        double nota1=0,
-               nota2=0,
-               nota3=0,
-               nota4=0,
-               nota5=0;
-               String media = null;
-               float media2 = 0;
-        cmd.comandos(nota1, nota2, nota3, nota4, nota5, valor, media, cursos, nota2);        
+        Comandos cmd = new Comandos();
+        byte op;
+        String op2;
+//        listas lst = new listas();
+        cmd.comandos();
+        op2 = JOptionPane.showInputDialog("Deseja repetir o procedimento ? \n1=Sim , 2=Não");
+        op = Byte.parseByte(op2.trim());
+        if (op == 1) {
+            main(args);
+        } else if (op == 2) {
+            System.console();
+        }
     }
 }
